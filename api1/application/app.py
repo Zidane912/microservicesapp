ca378754ee56e65c208c163e62af0c8cfaa83ed5
Zidane912 <alam.zidane912@gmail.com>
2022-05-13 10:31:29 +0000

    service 1

---
+++ api1/application/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, url_for
+import requests
+app = Flask(__name__)
+
+@app.route('/rpgrole/stat/desc', methods=['GET'])
+def get_rpgrole():
+    rpgrole = requests.get('http://localhost:5001/get/rpgrole').text # service 2
+    stat = requests.post('http://localhost:5002/post/rpgrole', data=rpgrole) # service 3
+    a_str = rpgrole + ',' + stat.text
+    desc = requests.post('http://localhost:5003/post/desc', data=a_str) #service 4
+    return render_template('home.html', title='Welcome', rpgrole=rpgrole, stat=stat.text, desc=desc.text)
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True, host='0.0.0.0')
