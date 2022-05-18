afdf9f361988ecc7911b771119053c1e730a7e69
Zidane912 <alam.zidane912@gmail.com>
2022-05-18 11:52:10 +0000

    spelling issues

--- api4/application/__init__.py
+++ api4/application/__init__.py
@@ -15,17 +15,17 @@ def post_desc():
     elif rpg_plus_stat[0] == 'Knight' and rpg_plus_stat[1] == 'HP: 2000 MP: 100':
         desc = 'Born from nobility, you resent the Hero as you think he stole your true role as the Hero. Special Trait: Super armor on heavy attacks.'
     elif rpg_plus_stat[0] == 'Healer' and rpg_plus_stat[1] == 'HP: 600 MP: 1000':
-        desc = 'Caring and Supportive, you support the team of the Hero with healing and stat boosts. Special Trait: Any allies in a 10 meter radius of you recivies regeneration of 3 percent health per second. Special Move: Guardian Touch: All allies have their stats boosted by 50%'
+        desc = 'Caring and Supportive, you support the team of the Hero with healing and stat boosts. Special Trait: Any allies in a 10 meter radius of you recivies regeneration of 3 percent health per second. Special Move: Guardian Touch: All allies have their stats boosted by 50%.'
     elif rpg_plus_stat[0] == 'Royal Jester' and rpg_plus_stat[1] == 'HP: 100 MP: 1':
-        desc = 'You have no special role, your job is simply to ammuse the royal king and thus you were exiled from the castle and you joined the party of the Hero. Special Move: Laugh of the Jester, if you make your enemies laugh they turn to stone'
+        desc = 'You have no special role, your job is simply to ammuse the royal king and thus you were exiled from the castle and you joined the party of the Hero. Special Move: Laugh of the Jester, if you make your enemies laugh they turn to stone.'
     elif rpg_plus_stat[0] == 'Thief' and rpg_plus_stat[1] == 'HP: 800 MP: 250':
-        desc = 'Quick and nible, your job is to steal from the caravan of the Hero. No special traits'
+        desc = 'Quick and nimble, your job is to steal from the caravan of the Hero. No special traits.'
     elif rpg_plus_stat[0] == 'Assassin' and rpg_plus_stat[1] == 'HP: 1000 MP: 300':
         desc = 'Silent and deadly, your role is to assassinate the royal king and other key figures you are employed to elimate. Special Trait 1: Invisibility, grants you to not be seen for a brief period of time. Special Trait 2: Slient footsteps'
     elif rpg_plus_stat[0] == 'Mage' and rpg_plus_stat[1] == 'HP: 300 MP: 3000':
-        desc = 'A strong intellect but a frail body, you have mastered the various disciplines of magic. Special Move: Magic Burst, expend all MP in an Area Of Effect Attack, attack power scales with remaining MP i.e. 100% will give more damage than 50% MP'
+        desc = 'A strong intellect but a frail body, you have mastered the various disciplines of magic. Special Move: Magic Burst, expend all MP in an Area Of Effect Attack, attack power scales with remaining MP i.e. 100% will give more damage than 50% MP.'
     elif rpg_plus_stat[0] == 'Metal Slime' and rpg_plus_stat[1] == 'HP: 5000 MP: 0':
-        desc = 'UNLUCKY, your role is simply to provide the Hero and his party with bonus EXP that is if they can hit you! Special Traits: 80,000 EXP, extremely high evasion'
+        desc = 'UNLUCKY, your role is simply to provide the Hero and his party with bonus EXP that is if they can hit you! Special Traits: 80,000 EXP, extremely high evasion.'
     else:
         desc =  desc = 'Born from Malice and Evil, your task is to kill the Hero and every other human you see. Special Trait: You cannot be harmed at all unless you are struck by excalibur, in which case you take twice the damage.'
     return Response(desc, mimetype='text/plain')   
